66b15986e28255ff0289a4498670688027c75afe
Semen <s.serdobincev@nv.dunice.net>
2023-04-07 13:13:55 +0300

    add 18st task

--- 9st task/main.js
+++ 9st task/main.js
@@ -1,14 +1,17 @@
+console.time('TIME')
 const triplet = function fn(n) {
   const ceil = n / 2;
-  for (let a = 1, b = 1; a <= ceil;) {
+  for (let a = 1, b = 1; a < ceil;) {
     let c = Math.sqrt(a * a + b * b);
     if (a + b + c === n) { 
       return a * b * c; 
     }
-    if ((b += 1) > ceil) { 
+    b += 1;
+    if (b > ceil) { 
       a += 1; b = 1; 
     }
   }
 };
 
 console.log(triplet(1000));
+console.timeEnd('TIME')
